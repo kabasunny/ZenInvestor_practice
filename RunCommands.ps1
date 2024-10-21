5bc17c40bd810cb3fa97c772a1adef1358bdbc85
kabasunny <dxbqd553@yahoo.co.jp>
2024-10-21 13:55:39 +0900

    'AddNewEnv'

--- RunCommands.ps1
+++
@@ -1,4 +0,0 @@
-# StartProjects.ps1
-Start-Process powershell -ArgumentList "Start-Process powershell -ArgumentList 'cd ./api-go; air; Pause'"
-Start-Process powershell -ArgumentList "Start-Process powershell -ArgumentList 'cd ./data-analysis-python/src; python app.py; Pause'"
-Start-Process powershell -ArgumentList "Start-Process powershell -ArgumentList 'cd ./frontend-react; npm start; Pause'"
--- RunDockerCompose.ps1
+++
@@ -1,8 +0,0 @@
-# RunDockerCompose.ps1
-Function RunDockerCompose {
-    Write-Host "Starting Docker containers..."
-    docker-compose up -d
-    Write-Host "Docker containers started."
-}
-
-RunDockerCompose
