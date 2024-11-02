23f58eea784729fc8a046989ff33a42c5411c620
kabasunny <dxbqd553@yahoo.co.jp>
2024-11-02 09:37:51 +0900

    MS用.env読み出しOK

--- api-go/src/service/ms_gateway/client/get_stock_data_client.go
+++ api-go/src/service/ms_gateway/client/get_stock_data_client.go
@@ -1,10 +1,12 @@
 package client
 
 import (
-	"api-go/src/service/ms_gateway/get_stock_data"
+	ms_gateway "api-go/src/service/ms_gateway/get_stock_data"
 	"context"
 	"fmt"
+	"os"
 
+	"github.com/joho/godotenv"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -23,13 +25,14 @@ type getStockDataClientImpl struct {
 
 // NewGetStockClient は GetStockClient の新しいインスタンスを作成
 func NewGetStockDataClient() (GetStockDataClient, error) {
-	// err := godotenv.Load()
-	// if err != nil {
-	//     return nil, fmt.Errorf("failed to load .env file: %w", err)
-	// }
-	// port := os.Getenv("GET_STOCK_MS_PORT")
-	// address := fmt.Sprintf("localhost:%s", port) // 環境変数からポート番号を取得
-	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	err := godotenv.Load()
+	if err != nil {
+		return nil, fmt.Errorf("failed to load .env file: %w", err)
+	}
+	port := os.Getenv("GET_STOCK_DATA_MS_PORT")
+	address := fmt.Sprintf("localhost:%s", port) // 環境変数からポート番号を取得
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		return nil, fmt.Errorf("failed to dial gRPC server: %w", err)
 	}
