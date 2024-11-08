243642159fe754b934f0a74e7b2b923e62e41743
kabasunny <dxbqd553@yahoo.co.jp>
2024-11-09 08:25:46 +0900

    Python側のテスト終了

--- data-analysis-python/src/calculate_indicator/simple_moving_average/simple_moving_average_grpc.py
+++ data-analysis-python/src/calculate_indicator/simple_moving_average/simple_moving_average_grpc.py
@@ -4,25 +4,25 @@ import simple_moving_average_pb2
 import simple_moving_average_pb2_grpc
 from simple_moving_average_service import calculate_simple_moving_average
 
-
 class SimpleMovingAverageService(
     simple_moving_average_pb2_grpc.SimpleMovingAverageServiceServicer
 ):
     def CalculateSimpleMovingAverage(self, request, context):
-        # リクエストから株価データを取り出し、float 型に変換
-        stock_data = [float(price) for price in request.stock_data]
-        # 移動平均を計算する関数を呼び出し、結果を values に格納
-        values = calculate_simple_moving_average(stock_data, request.window_size)
+        # 移動平均を計算する関数を呼び出し、結果を moving_average に格納
+        moving_average = calculate_simple_moving_average(request.stock_data, request.window_size)
+
         # 計算結果をレスポンスとして返す
+        response = simple_moving_average_pb2.SimpleMovingAverageResponse(
+            moving_average=moving_average
+        )
 
-        print("grpcサーバーが、calculate_simple_moving_averageサービスを呼び出し")
-        return simple_moving_average_pb2.SimpleMovingAverageResponse(values=values)
-
+        print("gRPCサーバーが、calculate_simple_moving_averageサービスを呼び出し")
+        return response
 
 def serve():
     # 最大10スレッドのスレッドプールを持つ gRPC サーバーを作成
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    # サーバーに MovingAverageService を追加
+    # サーバーに SimpleMovingAverageService を追加
     simple_moving_average_pb2_grpc.add_SimpleMovingAverageServiceServicer_to_server(
         SimpleMovingAverageService(), server
     )
@@ -33,7 +33,6 @@ def serve():
     print("Server started, listening on port 50053")
     # サーバー終了まで待機
     server.wait_for_termination()
-
 
 if __name__ == "__main__":
     serve()
